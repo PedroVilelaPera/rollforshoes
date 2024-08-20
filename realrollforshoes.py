c5c50b412e513fc4b5595927bae2b2e33fcbe935
Pedro Vilela Pereira <152188605+PedroPeraz@users.noreply.github.com>
2024-08-20 16:23:24 -0300

    Sistema de rolagem quase completo.

--- realrollforshoes.py
+++ realrollforshoes.py
@@ -32,6 +32,19 @@ import random
 
 jogadores = []
 
+def rolar_dado(quant_dado):
+    dado = 0
+    for x in range(quant_dado):
+        dado += (random.randint(1,6))
+        return dado
+    
+def criar_jogador():
+    global jogadores
+    print('\n')
+    nome = input(f'Insira o nome do jogador {x+1}: ')
+    jogador = {'Nome': nome,'Experiencia': 0,'Habilidades': [{'Fazer alguma coisa': 1}]}
+    jogadores.append(jogador)
+
 def mostrar_jogadores():
     print('\n')
     print(f'--> Aventureiros da mesa de {DM} <---')
@@ -46,75 +59,124 @@ def mostrar_jogadores():
                     print(f'{counter}. {chave} [{valor}]')
             print('\n')
 
-print('o---SUPER ROLL FOR SHOES---o')
-print('[1] Iniciar uma nova aventura!')
-print('[2] SAIR.')
-
-ato = int(input('Insira o número correspondente a ação que deseja fazer: '))
-
-os.system("cls")
-
-if ato == 1:
+while True:
     print('o---SUPER ROLL FOR SHOES---o')
-    print('\n')
-    DM = input(f'Insira o nome do Dungeon Master: ')
-    print('\n')
-    quant_jogadores = int(input('Insira a quantidade de jogadores: '))
-    print('\n')
-    for x in range(quant_jogadores):
-        nome = input(f'Insira o nome do jogador {x+1}: ')
-        jogador = {'Nome': nome,'Experiencia': 0,'Habilidades': [{'Fazer alguma coisa': 1}]}
-        jogadores.append(jogador)
+    print('[1] Iniciar uma nova aventura!')
+    print('[2] SAIR.')
 
-    while True:
-        os.system("cls")
-        print(f'--> Mesa de {DM} em andamento... <---')
-        print('[1] Rolar os dados.')
-        print('[2] Mostrar fichas')
-        print('[3] Encerrar Aventura.')
+    ato = int(input('Insira o número correspondente a ação que deseja fazer: '))
 
-        ato = int(input('Insira o número correspondente a ação que deseja fazer: '))
+    os.system("cls")
 
-        if ato == 1:
-            ficha = 0
+    if ato == 1:
+        print('o---SUPER ROLL FOR SHOES---o')
+        print('\n')
+        DM = input(f'Insira o nome do Dungeon Master: ')
+        print('\n')
+        quant_jogadores = int(input('Insira a quantidade de jogadores: '))
+        for x in range(quant_jogadores):
+            criar_jogador()
 
+        while True:
             os.system("cls")
-            mostrar_jogadores()
-            rolador = input('Insira o nome do jogador que irá rolar: ')
-            for x in jogadores:
-                while True:
-                    if rolador == x['Nome']:
-                        ficha = x                     
-                        break
-                    else:
-                        os.system("cls")
-                        mostrar_jogadores()
-                        print('[ERRO] Nome de jogador inválido.')
-                        rolador = input('Insira o nome do jogador presente na mesa que irá rolar: ')
+            print(f'--> Mesa de {DM} em andamento... <---')
+            print('[1] Rolar os dados.')
+            print('[2] Mostrar fichas.')
+            print('[3] Adicionar Jogadores.')
+            print('[4] Encerrar Aventura.')
 
-            print('\n')
+            ato = int(input('Insira o número correspondente a ação que deseja fazer: '))
 
-            counter = 0
-            print(f"[Habilidades de {ficha['Nome']}]")
-            for y in ficha['Habilidades']:
-                for chave,valor in y.items():
-                    counter += 1
-                    print(f'{counter}. {chave} [{valor}]')
+            if ato == 1:
+                ficha = 0
+                jogador_existe = False
+                os.system("cls")
+                print('[o-- ROLAR OS DADOS... --o]')
+                mostrar_jogadores()
+                rolador = input('Insira o nome do jogador que irá rolar: ')
+                for x in jogadores:
+                        if rolador == x['Nome']:
+                            jogador_existe = True
+                            ficha = x 
+                
+                if jogador_existe == False:
+                        while jogador_existe == False:
+                            os.system("cls")
+                            print('[o-- ROLAR OS DADOS... --o]')
+                            mostrar_jogadores()
+                            print('[ERRO] Nome de jogador inválido.')
+                            rolador = input('Insira o nome do jogador presente na mesa que irá rolar: ')
+                            for x in jogadores:
+                                if rolador == x['Nome']:
+                                    jogador_existe = True
+                                    ficha = x 
+                                    
+                os.system("cls")
+                counter = 0
+                print(f"[Habilidades de {ficha['Nome']}]")
+                for y in ficha['Habilidades']:
+                    for chave,valor in y.items():
+                        counter += 1
+                        print(f'[{counter}] {chave} | Nível = {valor}')
 
-            print('\n')
+                print('\n')
 
-            num_habilidade = int(input('Digite o número correspondente a habilidade: '))
-            posicao_interna = ficha['Habilidades']
-            habilidade_escolhida = posicao_interna[num_habilidade-1]
-            lista_habilidade = list(habilidade_escolhida)
-            chave_habilidade = lista_habilidade[0]
-            dado_habilidade = habilidade_escolhida[chave_habilidade]
+                num_habilidade = int(input('Digite o número correspondente a habilidade: '))
+                
+                #
+                habilidades = ficha['Habilidades']
+                habilidade_escolhida = habilidades[num_habilidade-1]
+                lista_habilidade = list(habilidade_escolhida)
+                chave_habilidade = lista_habilidade[0]
+                nivel_habilidade = habilidade_escolhida[chave_habilidade]
 
-            print(dado_habilidade)
-            
-            menu = input('Aperte qualquer tecla para voltar para o menu.')
+                dado_jogador = rolar_dado(nivel_habilidade)
 
-        elif ato == 2:
-            mostrar_jogadores() 
-            menu = input('Aperte qualquer tecla para voltar para o menu.')
+                print('\n')
+
+                print(f'[Dungeon Master {DM}]')
+                print('1 - Fácil')
+                print('2 - Normal')
+                print('3 - Difícil')
+                print('4 - Praticamente impossível')
+                dificuldade = int(input('Selecione o nível de dificuldade da ação: '))
+
+                dado_dm = rolar_dado(dificuldade)
+                print('\n')
+                if dado_jogador > dado_dm:
+                    print(f'Ação bem sucedida! Seu dado foi {dado_jogador}, em relação ao {dado_dm} do dado contrário.')
+                elif dado_jogador == dado_dm:
+                    print(f'Empate! Seu dado foi {dado_jogador}, em relação ao {dado_dm} do dado contrário.')
+                elif dado_jogador == 6 and nivel_habilidade == 1:
+                    pass
+                    #nova habilidade
+                else:
+                    print(f'Você falhou! Seu dado foi {dado_jogador}, em relação ao {dado_dm} do dado contrário.')
+                
+                print('\n')
+                input('Aperte qualquer tecla para voltar para o menu.')
+
+            elif ato == 2:
+                os.system('cls')
+                print('[o-- MOSTRAR FICHAS --o]')
+                mostrar_jogadores() 
+                input('Aperte qualquer tecla para voltar para o menu.')
+
+            elif ato == 3:
+                os.system('cls')
+                print('[o-- ADICIONAR JOGADORES --o]')
+
+                criar_jogador()
+
+            elif ato == 4:
+                print('\n')
+                print('Até a próxima aventura!')
+                time.sleep(3)
+                os.system('cls')
+                break 
     
+    elif ato == 2: 
+        print('See you traveler...')
+        time.sleep(3)
+        break
+
